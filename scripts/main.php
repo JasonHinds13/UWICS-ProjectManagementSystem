95fdf70608e697d413cfa6466faec51744058ed5
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2016-11-27 20:28:19 +0000

    Added code to create objects from POST requests

--- scripts/main.php
+++ scripts/main.php
@@ -25,9 +25,32 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $acctype = $_POST["acctype"];
     $password = $_POST["password"];
     
+    //create new member
     if(isset($fname) && isset($lname) && isset($id_num) && isset($email) && isset($sig) && isset($acctype) && isset($password)){
         $member = new Member($fname,$lname,$id_num,$email,$sig,$acctype,$password);
         $member->store_to_db();
+    }
+    
+    //post data for creating a new project
+    $pname = $_POST["proj_name"];
+    $pdesc = $_POST["proj_desc"];
+    $psig = $_POST["proj_sig"];
+    
+    //create new project
+    if(isset($pname) && isset($pdesc) &&isset($psig)){
+        $proj = new Project($pname,$pdesc,$psig);
+        $proj->store_to_db();
+    }
+    
+    //post data for creating new task
+    $tname = $_POST["t_name"];
+    $tdesc = $_POST["t_desc"];
+    $tmember = $_POST["t_mem"];
+    
+    //create new task
+    if(isset($tname) && isset($tdesc) && isset($tmember)){
+        $task = new Task($tname,$tdesc,$tmember,0);
+        $task->store_to_db();
     }
 }
 

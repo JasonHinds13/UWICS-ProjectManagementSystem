6a48d6b3103b2f3ff44c1b562c0ef94afcbc218c
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2016-11-27 20:08:04 +0000

    Fixed OOP code

--- scripts/main.php
+++ scripts/main.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once('/scripts/resourses.php');
+include_once('resourses.php');
 
 $host = getenv('IP');
 $username = getenv('C9_USER');
@@ -15,7 +15,20 @@ catch(PDOException $e){
 }
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
-    //handle post requests
+    
+    //post data for creating a new member
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $id_num = $_POST["id_num"];
+    $email = $_POST["email"];
+    $sig = $_POST["sig"];
+    $acctype = $_POST["acctype"];
+    $password = $_POST["password"];
+    
+    if(isset($fname) && isset($lname) && isset($id_num) && isset($email) && isset($sig) && isset($acctype) && isset($password)){
+        $member = new Member($fname,$lname,$id_num,$email,$sig,$acctype,$password);
+        $member->store_to_db();
+    }
 }
 
 if($_SERVER["REQUEST_METHOD"] === "GET"){
--- scripts/resourses.php
+++ scripts/resourses.php
@@ -4,31 +4,31 @@
 
 class Member{
     
-    private $fname = "";
-    private $lname = "";
-    private $id_num = "";
-    private $email = "";
-    private $sig = "";
-    private $acctype = "";
-    private $password = "";
+    protected $fname = "";
+    protected $lname = "";
+    protected $id_num = "";
+    protected $email = "";
+    protected $sig = "";
+    protected $acctype = "";
+    protected $password = "";
     
-    function __construct($fname,$lname,$id_num,$email,$sig,$acctype,$password){
-        $this->$fname = $fname;
-        $this->$lname = $lname;
-        $this->$id_num = $id_num;
-        $this->$email = $email;
-        $this->$sig = $sig;
-        $this->$acctype = $acctype;
-        $this->$password = $password;
+    public function __construct($fname,$lname,$id_num,$email,$sig,$acctype,$password){
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->id_num = $id_num;
+        $this->email = $email;
+        $this->sig = $sig;
+        $this->acctype = $acctype;
+        $this->password = $password;
     }
     
     public function store_to_db($conn){
-        if($this->$acctype == "leader"){
-            $sql = "INSERT INTO leaders(uwi_id,firstname,lastname,email,sig,password) VALUES('$this->$id_num','$this->$fname','$this->$lname','$this->$email','$this->$sig','$this->$password';);";
+        if($this->acctype == "leader"){
+            $sql = "INSERT INTO leaders(uwi_id,firstname,lastname,email,sig,password) VALUES('$this->id_num','$this->fname','$this->lname','$this->email','$this->sig','$this->password';);";
             $conn->exec($sql);
         }
         else{
-            $sql = "INSERT INTO users(uwi_id,firstname,lastname,email,sig,password) VALUES('$this->$id_num','$this->$fname','$this->$lname','$this->$email','$this->$sig','$this->$password';);";
+            $sql = "INSERT INTO users(uwi_id,firstname,lastname,email,sig,password) VALUES('$this->id_num','$this->fname','$this->lname','$this->email','$this->sig','$this->password';);";
             $conn->exec($sql);
         }
     }
@@ -36,52 +36,52 @@ class Member{
 
 class Project{
     
-    private $name = "";
-    private $desc = "";
-    private $sig = "";
+    protected $name = "";
+    protected $desc = "";
+    protected $sig = "";
     
-    private $tasks = array();
+    protected $tasks = array();
     
-    function __construct($name,$desc,$sig){
-        $this->$name = $name;
-        $this->$desc = $desc;
-        $this->$sig = $sig;
+    public function __construct($name,$desc,$sig){
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->sig = $sig;
     }
     
     public function store_to_db($conn){
-        $sql = "INSERT INTO projects(name,description,sig) VALUES('$this->$name','$this->desc','$this->$sig');";
+        $sql = "INSERT INTO projects(name,description,sig) VALUES('$this->name','$this->desc','$this->sig');";
         $conn->exec($sql);
     }
 }
 
 class Task{
     
-    private $name = "";
-    private $desc = "";
-    private $member = "";
-    private $progress = "";
+    protected $name = "";
+    protected $desc = "";
+    protected $member = "";
+    protected $progress = "";
     
-    function __construct($name,$desc,$member,$progress){
-        $this->$name = $name;
-        $this->$desc = $desc;
-        $this->$member = $member;
-        $this->$progress = $progress;
+    public function __construct($name,$desc,$member,$progress){
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->member = $member;
+        $this->progress = $progress;
     }
     
     public function store_to_db($conn){
-        $sql = "INSERT INTO tasks(name,description,member,progress) VALUES('$this->$name','$this->$desc','$this->member','$this->$progress');";
+        $sql = "INSERT INTO tasks(name,description,member,progress) VALUES('$this->name','$this->desc','$this->member','$this->progress');";
         $conn->exec($sql);
     }
 }
 
 class InterestGroup{
     
-    public $name = "";
-    public $leader = "";
+    protected $name = "";
+    protected $leader = "";
     
-    function __construct($name, $leader){
-        $this->$name = $name;
-        $this->$leader = $leader;
+    public function __construct($name, $leader){
+        $this->name = $name;
+        $this->leader = $leader;
     }
 }
 

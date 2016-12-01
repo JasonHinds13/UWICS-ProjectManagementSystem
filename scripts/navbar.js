99bd5f5afb46671767fa4291241e4af045c765a8
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2016-12-01 17:08:34 +0000

    Again

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var cproj = '<a href="createproject.html"><button id = "projects">Create Projects</button></a>'
     var vproj = '<a href="viewprojects.html"><button id = "projects">View Projects</button></a>'
     var vtask = '<a href="viewtasks.html"><button id = "tasks">View Tasks</button></a>'
-    var forum = '<a href="forum.php"><button id = "forum">Forum</button></a>'
+    var forum = '<a href="forum.html"><button id = "forum">Forum</button></a>'
     
     if (acctype == "leader") {
         //$("#home").attr('href','leaderhomepage.php');
